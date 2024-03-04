d88fe0587476cc1cfe79c1cf5b5ef8778e84fd43
LL <ludo4ka21@yandex.ru>
2024-03-05 00:51:01 +0300

    initial commit

---
+++ app/src/main/java/ru/ll/sign/Dialog.kt
@@ -0,0 +1,42 @@
+package ru.ll.sign
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.DialogFragment
+import ru.ll.sign.databinding.FragmentDialogBinding
+
+
+class Dialog : DialogFragment() {
+
+    private val filePicker = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri -> println("uri: $uri") }
+
+    var _binding: FragmentDialogBinding? = null
+    val binding: FragmentDialogBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textView.setOnClickListener {
+            filePicker.launch("*/*");
+        }
+    }
+}
